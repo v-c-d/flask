bba7ad875c61f300ac3fd43bdf223b71bf710ba8
Vittorio C <vittorio@chillemi.fr>
2021-02-11 23:15:02 +0100

    Move pip install to script

--- deployscripts/deploy.sh
+++ deployscripts/deploy.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 
+rm -rf ~/StatsApp
+mkdir -p ~/StatsApp
+cp -r /VSTSAgent/_work/r1/a/_v-c-d.flask/drop/ ~/StatsApp
+cd ~/StatsApp
+
+python3 -m venv env
+
+source env/bin/activate
+
+# pip install wheel
+# pip install uwsgi flask
+
+pip install -r requirements.txt
