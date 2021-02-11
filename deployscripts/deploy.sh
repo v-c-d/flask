ca38185c246913357fed68babe90a432da0205f4
Vittorio C <vittorio@chillemi.fr>
2021-02-11 23:22:19 +0100

    azdaz

--- deployscripts/deploy.sh
+++ deployscripts/deploy.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
+echo 'HHHHHHHHHHHHHHHHHEEEEEEEEEEELLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOO'
+whoami
+ls -al
+pwd
 rm -rf ~/StatsApp
 mkdir -p ~/StatsApp
 cp -r /VSTSAgent/_work/r1/a/_v-c-d.flask/drop/ ~/StatsApp
 cd ~/StatsApp
+ls -al
+pwd
 
 python3 -m venv env
 
 source env/bin/activate
+ls -al
+pwd
 
 # pip install wheel
 # pip install uwsgi flask
 
-pip install -r requirements.txt
+pip install -r /VSTSAgent/_work/r1/a/_v-c-d.flask/drop/requirements.txt
+ls -al
+pwd
