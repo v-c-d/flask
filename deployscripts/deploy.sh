3dcebbf7bb7d00dc99f6e59af92c178c0d88253e
Vittorio C <vittorio@chillemi.fr>
2021-02-12 10:55:35 +0100

    App structure

--- deployscripts/deploy.sh
+++ deployscripts/deploy.sh
@@ -9,16 +9,11 @@ rm -rf ~/StatsApp
 
 cp -r /VSTSAgent/_work/r1/a/_v-c-d.flask/drop/ ~/StatsApp
 cd ~/StatsApp
-ls -al
 
 python3 -m venv env
 source env/bin/activate
 
-# pip install wheel
-# pip install uwsgi flask
-
 pip install -r requirements.txt
-ls -al
 EOF
 echo "Out"
 whoami
